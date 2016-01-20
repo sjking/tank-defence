543d88634958a93de0940c46f66fbed24177b594
sjking <stevejohnking@gmail.com>
2016-01-19 16:10:55 -0800

    Set the status bar position based on the canvas height

--- src/StatusBar.js
+++ src/StatusBar.js
@@ -2,20 +2,20 @@ var StatusBar = {
     init: function(context, player) {
         this.context = context;
         this.player = player;
-        this.ground = 416; // TO-DO: this shouldn't be hard-coded
+        this.posY = this.context.canvas.height - 64;
     },
     draw: function() {
         this.context.fillStyle = "#eee";
         this.context.font = "12pt Arial";
         this.context.textAlign = "left";
-        this.context.fillText('Power: ', 10, this.ground+25);
-        this.context.fillText('Lives: ', 10, this.ground+50);
-        this.context.fillText(this.player.lives, 60, this.ground+50);
+        this.context.fillText('Power: ', 10, this.posY + 25);
+        this.context.fillText('Lives: ', 10, this.posY + 50);
+        this.context.fillText(this.player.lives, 60, this.posY + 50);
         this.context.fillStyle = "#666";
-        this.context.fillRect(70, this.ground+15, 100, 10);
+        this.context.fillRect(70, this.posY + 15, 100, 10);
         this.context.fillStyle = "#ddd";
         // current power
-        this.context.fillRect(70, this.ground+15, 
+        this.context.fillRect(70, this.posY + 15, 
                 this.player.canonBallSpeed*6.67, 10);
         this.context.fillStyle = "#000";
     }
